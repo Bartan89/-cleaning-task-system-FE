c450fb5f0a1a918d2ec651ca0ab26fd6a7504a66
Bart Kuijper <bartkuijper@Barts-Air.zyxel.box>
2021-05-09 23:12:24 +0200

    loggie

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.API_URL || "https://cleaning-task-system-be.herokuapp.com/"
+export const apiUrl = process.env.API_URL || "https://cleaning-task-system-be.herokuapp.com"
 export const DEFAULT_PAGINATION_LIMIT = 50
 export const DEFAULT_MESSAGE_TIMEOUT = 3000
